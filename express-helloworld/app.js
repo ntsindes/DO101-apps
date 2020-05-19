1a16edad1a9e534c09957d55948270f5e4175125
ntsindes <sivuyile.ntsinde@live.co.za>
2020-05-19 15:11:31 +0200

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function(req,res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/sivu', function(req,res) {
+  res.send('Hello Sivu!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
